72b24ca5dbc0d814cc37e4f1ab5784eeff1f9420
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-02-02 21:24:37 -0300

    teste

---
+++ Rede/Rede.Test/UnitTest/Entity/RedeTest.cs
@@ -0,0 +1,27 @@
+using domain = Rede.Domain.Entity;
+
+namespace Rede.Test.Entity;
+
+public class RedeTest
+{
+    [Fact(DisplayName = nameof(AtivarTest))]
+    [Trait("Domain","Rede")]
+    public void AtivarTest()
+    {
+        domain.Rede rede = new();
+        rede.Ativar();
+        
+        Assert.Equal("S",rede.ST_REDE);
+        
+    }
+
+    [Fact(DisplayName = nameof(DesativarTest))]
+    [Trait("Domain", "Rede")]
+    public void DesativarTest()
+    {
+        domain.Rede rede = new();
+        rede.Desativar();
+        
+        Assert.Equal("N",rede.ST_REDE);
+    }
+}
---
+++ Rede/Rede.Test/UnitTest/Entity/UnidadeTest.cs
@@ -0,0 +1,25 @@
+using domain = Rede.Domain.Entity;
+namespace Rede.Test.Entity;
+
+public class UnidadeTest
+{
+    [Fact(DisplayName = nameof(AtivarUnidadeTest))]
+    [Trait("Domain", "Unidade")]
+    public void AtivarUnidadeTest()
+    {
+        domain.Unidade unidade = new domain.Unidade();
+        unidade.Ativar();
+        
+        Assert.Equal("S",unidade.ST_UNIDADE);
+    }
+
+    [Fact(DisplayName = nameof(DesativarUnidadeTest))]
+    [Trait("Domain", "Unidade")]
+    public void DesativarUnidadeTest()
+    {
+        domain.Unidade unidade = new domain.Unidade();
+        unidade.Desativar();
+        
+        Assert.Equal("N",unidade.ST_UNIDADE);
+    }
+}
