50ce4b93f0a38cdae87f5a0d42a336539d3d5ee7
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-02-19 22:06:43 -0300

    criação projeto teste e2e

---
+++ Rede/Rede.E2ETest/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Rede.E2ETest.Base;
+//criação de um servidor web para testes E2E
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        //configuração especifica para testes
+        var environmentName = "EndToEndTest"; 
+        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT",environmentName);
+        builder.UseEnvironment(environmentName);
+        builder.ConfigureServices(services =>
+        {
+
+        });
+        base.ConfigureWebHost(builder);
+    }
+}
