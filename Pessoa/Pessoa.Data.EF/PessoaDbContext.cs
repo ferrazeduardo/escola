04708743f4fd58baa5dd198051f7e8ed024b99fc
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-03-19 22:06:32 -0300

    dbset telefone

--- Pessoa/Pessoa.Data.EF/PessoaDbContext.cs
+++ Pessoa/Pessoa.Data.EF/PessoaDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Pessoa.Data.EF.Configurations;
+using Pessoa.Domain.Entity;
 
 namespace Pessoa.Data.EF;
 
 public class PessoaDbContext  : DbContext
 {
     public DbSet<Domain.SeedWorks.Pessoa> Pessoas => Set<Domain.SeedWorks.Pessoa>();
+    public DbSet<Telefone> Telefone => Set<Telefone>();
 
     public PessoaDbContext(DbContextOptions<PessoaDbContext> options) : base(options)
     {
@@ -15,6 +17,7 @@ public class PessoaDbContext  : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new PessoaConfiguration());
+        modelBuilder.ApplyConfiguration(new TelefoneConfiguration());
         //base.OnModelCreating(modelBuilder);
     }
 }
