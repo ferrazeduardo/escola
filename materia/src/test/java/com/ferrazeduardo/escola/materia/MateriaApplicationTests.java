349fbcb749217451acf65bdec7d0bdf3665fe761
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-04-15 10:52:23 -0300

    api materia

---
+++ materia/src/test/java/com/ferrazeduardo/escola/materia/MateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferrazeduardo.escola.materia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
