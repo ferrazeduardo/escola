6166f13afc4942f8722dd52b6957df3b08f3e2b3
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-04-16 11:58:15 -0300

    record materia input

--- materia/src/main/java/com/ferrazeduardo/escola/materia/controller/MateriaController.java
+++ materia/src/main/java/com/ferrazeduardo/escola/materia/controller/MateriaController.java
@@ -1,15 +1,14 @@
 package com.ferrazeduardo.escola.materia.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ferrazeduardo.escola.materia.domain.DataTransferObject.MateriaInput;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/materia")
 public class MateriaController {
 
-    @GetMapping
-    public String SalvarMateria(){
-        return "Materia Salva java";
+    @PostMapping
+    public MateriaInput SalvarMateria(@RequestBody MateriaInput materiaInput){
+        return materiaInput;
     }
 }
---
+++ materia/src/main/java/com/ferrazeduardo/escola/materia/domain/DataTransferObject/MateriaInput.java
@@ -0,0 +1,4 @@
+package com.ferrazeduardo.escola.materia.domain.DataTransferObject;
+
+public record MateriaInput(String descricao, int id_rede){
+}
