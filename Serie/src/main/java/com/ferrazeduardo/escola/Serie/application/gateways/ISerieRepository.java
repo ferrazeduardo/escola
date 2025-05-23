3dd276de1dbfb74428c8aa7317c9d0e52e94c9a3
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2025-05-23 00:28:38 -0300

    interface serie repository

---
+++ Serie/src/main/java/com/ferrazeduardo/escola/Serie/application/gateways/ISerieRepository.java
@@ -0,0 +1,6 @@
+package com.ferrazeduardo.escola.Serie.application.gateways;
+import com.ferrazeduardo.escola.Serie.domain.Serie;
+
+public interface ISerieRepository {
+    void Inserir(Serie serie);
+}
