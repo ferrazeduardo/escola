51eaacac685c4ab9a7e0b37fd9df8d562856f373
eduardo <eduardo.borges.ferraz@gmail.com>
2025-07-13 22:49:44 -0300

    db em index.js

--- microservices/Rede/InserirRedeApiPessoa/index.js
+++ microservices/Rede/InserirRedeApiPessoa/index.js
@@ -1,6 +1,7 @@
 const { rabbitmqUrl, queueName } = require('./config/env');
 const startConsumer = require('./queue/consumer');
 const runConsumer = require('./app/runConsumer');
+const db = require('./db/pgClient');
 
 (async () => {
     await runConsumer({ rabbitmqUrl, queueName, db });
