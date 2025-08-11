2adbda3e0950ffc5afb2f1a8a1bf4f5f0749cb7f
eduardo <eduardo.borges.ferraz@gmail.com>
2025-08-10 23:32:16 -0300

    api aula

---
+++ Aula/src/test/java/com/ferrazeduardo/escola/Aula/AulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferrazeduardo.escola.Aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
