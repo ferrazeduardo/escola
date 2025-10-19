f5071e90217db17d580c1e0d61fb95f66d51d031
eduardo <eduardo.borges.ferraz@gmail.com>
2025-10-19 13:15:27 -0300

    validador de regra

--- Usuario/Usuario.Domain/Exception/ExcecaoDeDominio.cs
+++ Usuario/Usuario.Domain/Exception/ExcecaoDeDominio.cs
@@ -2,8 +2,17 @@ namespace Usuario.Domain.Exception;
 
 public class ExcecaoDeDominio : System.Exception
 {
+    public string Mensagem { get; set; }
+
     public ExcecaoDeDominio(string mensagem) : base(mensagem)
     {
         
+    }
+    
+
+    public void HaError(bool temErro, string mensagem)
+    {
+        if (temErro)
+            throw new System.Exception(mensagem);
     }
 }
--- Usuario/Usuario.Domain/Validation/ValidadorDeRegra.cs
+++ Usuario/Usuario.Domain/Validation/ValidadorDeRegra.cs
@@ -1,8 +1,31 @@
 using System;
+using Usuario.Domain.Exception;
 
 namespace Usuario.Domain.Validation;
 
 public class ValidadorDeRegra
 {
+    private string Mensagens = "";
+    public ValidadorDeRegra()
+    {
+    }
 
+    public static ValidadorDeRegra Novo()
+    {
+        return new ValidadorDeRegra();
+    }
+
+    public ValidadorDeRegra Quando(bool temErro, string mensagem)
+    {
+        if (temErro)
+            Mensagens = mensagem;
+
+        return this;
+    }
+
+    public void DispararExcecaoSeExistir()
+    {
+        if(string.IsNullOrEmpty(Mensagens))
+           throw new ExcecaoDeDominio(Mensagens);
+    }
 }
