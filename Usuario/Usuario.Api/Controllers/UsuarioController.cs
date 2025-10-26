9446ae15639878d6909dfdaa6528fb825e104f26
eduardo <eduardo.borges.ferraz@gmail.com>
2025-10-26 13:22:10 -0300

    usuario controller

---
+++ Usuario/Usuario.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuario.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        
+
+        public UsuarioController()
+        {
+            
+        }
+    }
+}
