4ed69f6369fbc92291c1d9df213d7a5c8403ab67
eduardo <eduardo.borges.ferraz@gmail.com>
2025-12-21 23:44:32 -0300

    esboco menu

--- FrontEscola/escola/src/app/layout/header/header.component.ts
+++ FrontEscola/escola/src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+   imports: [MatButtonModule, MatMenuModule],
 })
 export class HeaderComponent implements OnInit {
 menu: any;
