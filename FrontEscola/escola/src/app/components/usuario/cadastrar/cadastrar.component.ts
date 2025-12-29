ceb36fcd83b3def384908fce15c93837d7f1c354
eduardo <eduardo.borges.ferraz@gmail.com>
2025-12-28 21:23:04 -0300

    formulario cadastrar usuario

--- FrontEscola/escola/src/app/components/usuario/cadastrar/cadastrar.component.ts
+++ FrontEscola/escola/src/app/components/usuario/cadastrar/cadastrar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
+  standalone: false,
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
+  styleUrls: ['./cadastrar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CadastrarComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(
+   private fb: FormBuilder
+  ) {
+     this.form = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      dataNascimento: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      login: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
+   }
 
   ngOnInit() {
+  }
+
+
+
+  salvar(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const usuario = this.form.value;
+
+    console.log('UsuarioSaveInput:', usuario);
+
+    // aqui você chama o backend
   }
 
 }
--- FrontEscola/escola/src/app/components/usuario/usuario.module.ts
+++ FrontEscola/escola/src/app/components/usuario/usuario.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioRoutes } from './usuario.routing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    UsuarioRoutes
+    UsuarioRoutes,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    ReactiveFormsModule
   ],
-  declarations: []
+  declarations: [CadastrarComponent]
 })
 export class UsuarioModule { }
