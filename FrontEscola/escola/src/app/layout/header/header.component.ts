e566cb135eb0ff0f0e201d124f1a46837d253ec0
eduardo <eduardo.borges.ferraz@gmail.com>
2025-12-28 22:18:55 -0300

    menu usuario - cadastrar

--- FrontEscola/escola/src/app/layout/header/header.component.ts
+++ FrontEscola/escola/src/app/layout/header/header.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-   imports: [MatButtonModule, MatMenuModule],
+  imports: [MatButtonModule, MatMenuModule, RouterModule],
 })
 export class HeaderComponent implements OnInit {
-menu: any;
+  menu: any;
 
   constructor() { }
 
