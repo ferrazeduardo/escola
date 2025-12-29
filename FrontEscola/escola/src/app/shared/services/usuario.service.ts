dcfba1446c60401b077dcee96ac30fada390cd71
eduardo <eduardo.borges.ferraz@gmail.com>
2025-12-28 22:38:57 -0300

    usuario service

--- FrontEscola/escola/src/app/shared/services/usuario.service.ts
+++ FrontEscola/escola/src/app/shared/services/usuario.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { ApibaseService } from './apibase.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  
-constructor() { }
+  protected baseApiPath = environment.baseApiPath;
+  constructor(
+    private apiBase: ApibaseService
+  ) { }
 
+  cadastrar(param: any) {
+    const uri: string = '/usuario/cadastrar';
+    return this.apiBase.post(`${this.baseApiPath + uri}`, undefined, param);
+  }
 }
