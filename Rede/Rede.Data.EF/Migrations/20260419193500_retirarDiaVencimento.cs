86b73e52ed00915acd1b5b3d5dd437729683cb9a
eduardo <eduardo.borges.ferraz@gmail.com>
2026-04-19 16:35:24 -0300

    migrations

---
+++ Rede/Rede.Data.EF/Migrations/20260419193500_retirarDiaVencimento.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Rede.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class retirarDiaVencimento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaVencimento");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaVencimento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ID_REDE = table.Column<int>(type: "integer", nullable: false),
+                    Dia = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaVencimento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_REDE_DIAVENCIMENTO",
+                        column: x => x.ID_REDE,
+                        principalTable: "Rede",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiaVencimento_ID_REDE",
+                table: "DiaVencimento",
+                column: "ID_REDE");
+        }
+    }
+}
