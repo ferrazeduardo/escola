a15e0858c9d19ef3d89e4cc850e5c9cd9c153bba
eduardo <eduardo.borges.ferraz@gmail.com>
2026-06-06 21:56:26 -0300

    injecao dependencia Iipessoarepository com pessoarepository

--- Academico/Academico.Data.EF/DependencyInjection/DependencyInjection.cs
+++ Academico/Academico.Data.EF/DependencyInjection/DependencyInjection.cs
@@ -1,8 +1,20 @@
 using System;
+using Academico.Data.EF.Repository;
+using Academico.Domain.Interface.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Academico.Data.EF.DependencyInjection;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
+
+    public static IServiceCollection AddDataEf(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IPessoaRepository, PessoaRepository>();
+        
+
+        return services;
+    }
 
 }
