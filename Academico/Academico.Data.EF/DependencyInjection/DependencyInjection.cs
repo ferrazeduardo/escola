fb0111410df89a2914d7b6b742fc21b4416d104e
eduardo <eduardo.borges.ferraz@gmail.com>
2026-06-06 22:02:49 -0300

    Injecao de dependencia unitofwark

--- Academico/Academico.Data.EF/DependencyInjection/DependencyInjection.cs
+++ Academico/Academico.Data.EF/DependencyInjection/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using System;
 using Academico.Data.EF.Repository;
+using Academico.Domain.Interface;
 using Academico.Domain.Interface.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,7 +14,7 @@ public static class DependencyInjection
     public static IServiceCollection AddDataEf(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IPessoaRepository, PessoaRepository>();
-
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddDbContext<AcademicoDbContext>(options =>
         options.UseNpgsql(configuration.GetConnectionString("defaultConnection")));
 
