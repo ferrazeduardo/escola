7a6d7e4124eff87b2458f93c4f07c5b3102b422b
eduardo <eduardo.borges.ferraz@gmail.com>
2026-06-28 17:35:35 -0300

    validacao materia e status materia, metodo ativar materia

--- Academico/Academico.Domain/Entity/Materia.cs
+++ Academico/Academico.Domain/Entity/Materia.cs
@@ -1,9 +1,33 @@
 using System;
+using Academico.Domain.Exception;
 using Academico.Domain.SeedWork;
 
 namespace Academico.Domain.Entity;
 
 public class Materia : AggregateRoot
 {
+
+    public Materia(string ds_materia)
+    {
+        DS_MATERIA = ds_materia;
+        Validacao();
+    }
+
+    public Materia()
+    {
+        
+    }
+
     public String DS_MATERIA { get; set; }
+    public Boolean ST_MATERIA { get; set; }
+
+    public void Ativar()
+    {
+        ST_MATERIA = true;
+    }
+
+    public void Validacao()
+    {
+        ExcecaoDeDominio.HaError(string.IsNullOrEmpty(DS_MATERIA), "Descrição da matéria é obrigatório.");
+    }
 }
