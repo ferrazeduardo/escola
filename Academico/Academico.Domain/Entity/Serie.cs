f3e5ca2d52c8f1288bcaa55d1ea3b45edc7848b7
eduardo <eduardo.borges.ferraz@gmail.com>
2026-07-12 18:53:56 -0300

    remove methods for managing periodos and unidades salas in Serie entity

--- Academico/Academico.Domain/Entity/Serie.cs
+++ Academico/Academico.Domain/Entity/Serie.cs
@@ -17,29 +17,4 @@ public class Serie : AggregateRoot
 
     public ICollection<SerieUnidadeSala> _serieunidadeSalas { get; private set; } = [];
 
-    public void AdicionarPeriodo(int idPeriodo)
-    {
-        if (!periodosId.Contains(idPeriodo))
-            periodosId.Add(idPeriodo);
-    }
-
-    public void AdicionarUnidadeSala(int idUnidade, string nrSala)
-    {
-        if (!unidadesIdSalas.Any(x => x.Item1 == idUnidade && x.Item2 == nrSala))
-            unidadesIdSalas.Add((idUnidade, nrSala));
-    }
-
-
-    public void RemoverPeriodo(int idPeriodo)
-    {
-        if (periodosId.Contains(idPeriodo))
-            periodosId.Remove(idPeriodo);
-    }
-
-    public void RemoverUnidadeSala(int idUnidade, string nrSala)
-    {
-        var item = unidadesIdSalas.FirstOrDefault(x => x.Item1 == idUnidade && x.Item2 == nrSala);
-        if (item != default)
-            unidadesIdSalas.Remove(item);
-    }
 }
