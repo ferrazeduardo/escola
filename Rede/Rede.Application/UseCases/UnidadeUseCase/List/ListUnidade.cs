6146dec6d28571e4ab20a5c957678424cf5c9643
eduardo <eduardo.borges.ferraz@gmail.com>
2026-09-08 23:40:54 -0300

    feat: implement ListUnidade use case by adding Handle method and output mapping

--- Rede/Rede.Application/UseCases/UnidadeUseCase/List/ListUnidade.cs
+++ Rede/Rede.Application/UseCases/UnidadeUseCase/List/ListUnidade.cs
@@ -1,5 +1,6 @@
 using System;
 using MediatR;
+using Rede.Domain.Exception;
 using Rede.Domain.Interfaces.Repository;
 
 namespace Rede.Application.UseCases.UnidadeUseCase.List;
@@ -14,6 +15,11 @@ public class ListUnidade : IRequestHandler<ListUnidadeInput, ListUnidadeOutput>
     }
     public async Task<ListUnidadeOutput> Handle(ListUnidadeInput request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+       var unidades = await _unidadeRepository.ListarTodosPorRede(request.id_rede);
+       NotFounException.IsNull(unidades, "Nenhuma unidade encontrada");
+
+       var output = new ListUnidadeOutput();
+         output.FromEntity(unidades);
+         return output;
     }
 }
--- Rede/Rede.Application/UseCases/UnidadeUseCase/List/ListUnidadeOutput.cs
+++ Rede/Rede.Application/UseCases/UnidadeUseCase/List/ListUnidadeOutput.cs
@@ -1,9 +1,23 @@
 using System;
 using Rede.Application.UseCases.UnidadeUseCase.Common;
+using Rede.Domain.Entity;
 
 namespace Rede.Application.UseCases.UnidadeUseCase.List;
 
 public class ListUnidadeOutput
 {
     public List<ModelUnidadeOutput> unidades { get; set; }
+
+    internal void FromEntity(List<Unidade> unidades)
+    {
+        this.unidades = unidades.Select(x => new ModelUnidadeOutput
+        {
+            id = x.Id,
+            endereco = x.DS_ENDERECO,
+            numero = x.NR_UNIDADE,
+            complemento = x.DS_COMPLMENTO,
+            cep = x.NR_CEP,
+            status = x.ST_UNIDADE
+        }).ToList();
+    }
 }
